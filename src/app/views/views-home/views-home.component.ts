c7e57c4b9ab098c64d424a404b3f768490c98886
Robert Constantinescu <robertsci@github.com>
2021-01-10 19:21:21 +0200

    finishing the Views component

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewsHomeComponent implements OnInit {
 
+  stats = [
+    { value: 22, label: 'Users'},
+    { value: 9000, label: 'Revenue'},
+    { value: 85, label: 'Reviews'}
+  ];
+
+  items = [
+    {image: '/assets/images/couch.jpeg', title: 'Couch', description: 'Confy Couch'},
+    {image: '/assets/images/dresser.jpeg', title: 'Dresser', description: 'Big Dresser'},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
